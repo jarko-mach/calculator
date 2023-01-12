8ba0461f512d08c1af85b54b958ffeb37d5a4e82
Jarko Mach <jarko.mach@gmail.com>
2023-01-12 08:45:58 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,30 @@
+console.log("Kilo sera")
+
+const add = (a, b) => {
+    return [a + b, '+'];
+}
+
+const sub = (a, b) => {
+    return [a - b, "-"];
+}
+
+const multi = (a, b) => {
+    return [a * b, "*"];
+}
+
+const div = (a, b) => {
+    return [a / b, "/"];
+}
+
+const a = 22
+const b = 4
+
+const consoleResult = (a, b, action) => {
+    var result = a + " " + action[1] + " " + b + " = " + action[0]
+    console.log(result)
+}
+
+consoleResult(a, b, add(a, b))
+consoleResult(a, b, sub(a, b))
+consoleResult(a, b, multi(a, b))
+consoleResult(a, b, div(a, b))
